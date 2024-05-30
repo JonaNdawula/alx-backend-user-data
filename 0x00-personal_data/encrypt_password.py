c423dcdfab1267d2c5c70b522bbb3ac972f6d752
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-30 06:35:36 -0700

    code update

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,6 +14,7 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     This function is used to validate
