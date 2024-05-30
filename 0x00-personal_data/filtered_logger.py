847b1b4dfe615f1ac039cff60fafe1b60fbf2a22
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-05-30 02:43:28 -0700

    code for 1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,7 @@ obfuscated
 """
 
 import re
+import logging
 
 
 def filter_datum(
@@ -19,3 +20,26 @@ def filter_datum(
         message = re.sub(f"{field}=[^{separator}]*",
                          f"{field}={redaction}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """
+        initializer
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Function to format
+        """
+        o_msg = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, o_msg, self.SEPARATOR)
