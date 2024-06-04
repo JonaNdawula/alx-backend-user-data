8d6bf56a57554ebe242a1a81ce03cecebbc9c4f8
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-04 08:17:49 -0700

    code for 8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ This module contains functions
 for basic authentication
 """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(
+                                           self,
+                                           base64_authorization_header: str
+                                          ) -> str:
+        """
+        A function thatdecodes a pass
+        """
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
