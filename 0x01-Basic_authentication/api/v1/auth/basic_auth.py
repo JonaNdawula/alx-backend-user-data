e40b83bf662960200f1b46e9aaad67478387d8f9
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-04 11:31:07 -0700

    code for 9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,7 +30,7 @@ class BasicAuth(Auth):
                                            base64_authorization_header: str
                                           ) -> str:
         """
-        A function thatdecodes a pass
+        A function that decodes
         """
         if base64_authorization_header is None:
             return None
@@ -41,3 +41,23 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+                                 self,
+                                 decoded_base64_authorization_header: str
+                                ) -> (str, str):
+        """
+        A function that returns
+        the user email and password
+        from Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_email, user_password = decoded_base64_authorization_header.split(
+                                    ':', 1
+                                    )
+        return user_email, user_password
