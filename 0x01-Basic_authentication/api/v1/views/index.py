174caccdbeb08d784abb82321b1727226bd7efa6
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-04 05:12:51 -0700

    code for 1. Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,3 +25,10 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    This end point must raise a 401 error.
+    """
+    abort(401)
