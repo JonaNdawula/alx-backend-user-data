ee59b9fa4836efb2fcfdfb59d6f32da0af56851c
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-06 05:17:07 -0700

    1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This modulehas classes and methods used
+for session authenntication
+"""
+
+from api.v1.auth.auth import Auth
+
+class SessionAuth:
+    """
+    This class inherits from Auth
+    """
+    pass
