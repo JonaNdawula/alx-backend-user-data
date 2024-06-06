04f27f6471966ab0e0041f54aa4f3b278eb939c6
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-06 06:22:33 -0700

    code for 2. Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 """
-This modulehas classes and methods used
+This module has classes and methods used
 for session authenntication
 """
 from api.v1.auth.auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """
     This class inherits from Auth
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Method that creates a session
+        """
+        if user_id is None or type(user_id) != str:
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
