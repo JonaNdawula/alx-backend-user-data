6e4dce0db63a835a685a4929a72677eed7554d9e
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-12 02:19:06 -0700

    code update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 This module contains a flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -31,6 +31,26 @@ def users():
         return jsonify({'email': email, 'message': 'user created'})
     except ValueError:
         return jsonify({'message': 'email already resgistered'}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    Login function
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    response = make_response(
+               jsonify({'email': email, 'message': 'logged in'}), 200)
+    response.set_cookie('session_id', session_id)
+
+    return response
 
 
 if __name__ == "__main__":
