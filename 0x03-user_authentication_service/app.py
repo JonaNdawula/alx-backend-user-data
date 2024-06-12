4d3877352de07e11c87ee97bef951e0ee3e2070c
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-12 06:38:11 -0700

    code update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 This module contains a flask app
 """
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
 
