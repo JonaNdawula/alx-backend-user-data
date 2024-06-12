721e8a47f5856ef5e1a0dfb1d50ad6cd7b494d84
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-12 06:23:25 -0700

    code for app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,5 +53,21 @@ def login():
     return response
 
 
+@app.route('/session', methods=['DELETE'])
+def logout():
+    """
+    Logout function
+    """
+    session_id = request.cokkies.get('session_id')
+
+    user = auth.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+    auth.destroy_session(user.id)
+
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
