86bf86a2872827b7b90ff1069369ce980445c044
Jonathan Ndawula <jonathan.ndawula@yahoo.com>
2024-06-12 07:46:47 -0700

    update app.y

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,5 +87,19 @@ def profile():
     return jsonify({'email': user.email}), 200
 
 
+@app.route('/reset_password', methods=['Post'], strict_slashes=False)
+def get_rest_password_token():
+    """
+    Responds to POST /reset_password route
+    """
+    email = request.form.get('email')
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({'email': email, 'reset_token': reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
